11ab103c3114070ad8b84a934862a8d5be09aea1
Alberto Barradas <abcsds@gmail.com>
2015-02-20 09:52:16 -0600

    Declared list methods

--- list.h
+++ list.h
@@ -10,8 +10,17 @@ private:
     node<T> *current;
     int size;
 public:
-    list () { head = tail = NULL; }
-    virtual ~list () {  if(!isEmpty()) deleteList(); }
+    list () {
+        size = 0;
+        head = tail = NULL;
+    }
+    virtual ~list () {
+        if(!isEmpty()) {
+            while (head!=NULL) {
+                pop();
+            }
+        }
+    }
 
     node<T>* head() { return head; }
     node<T>* tail() { return tail; }
@@ -20,40 +29,93 @@ public:
     bool isEmpty() { return (head==NULL) }
     int size() {
         int len = 0;
+        // TODO check if starting at 1 or 0
         node<T>* aux;
         for(aux=head;aux;aux = aux->next()) len++;
         size = len;
         return size;
     }
     /* TODO:
-        head -> inserts front  -- return node<T>*?
-        tail -> inserts back
-        push -> use insert back
-        pop -> remove back, return back data
 
-        frontPop
-        frontPush -> head
+        bool has(T data);
+        node<T>* find(T data);
+        bool delete(node<T>* unwanted);
+        search data
+        search position
+        overload []
     */
-    void head(T data) {
+    void insertBack(T data) {
         node<T>* newNodeP;
         newNodeP = new node<T>(data);
+        // TODO check for errors in memory asignment
+        if (isEmpty()) {
         // if inserting first node:
-        if (head==NULL) {
-
+            head = newNodeP;
+            tail = newNodeP;
+            newNodeP->prev(NULL);
+            newNodeP->next(NULL);
+            size=1;
         }
         else {
-            /* code */
+            tail->next(newNodeP);
+            newNodeP->prev(tail);
+            newNodeP->next(NULL);
+            tail = newNodeP;
+            size++;
         }
     }
-    void tail(T data) {
+    void insertFront(T data) {
         node<T>* newNodeP;
         newNodeP = new node<T>(data);
-        // if inserting first node:
+        // TODO check for errors in memory asignment
         if (head==NULL) {
-
+        // if inserting first node:
+            head = newNodeP;
+            tail = newNodeP;
+            newNodeP->prev(NULL);
+            newNodeP->next(NULL);
+            size=1;
         }
         else {
-            /* code */
+            head->prev(newNodeP);
+            newNodeP->next(head);
+            newNodeP->prev(NULL);
+            head = newNodeP;
+            size++;
+        }
+    }
+    void push(T data) {
+        insertBack(T data);
+    }
+    node<T>* pop() {
+        if (isEmpty()) {
+            return NULL;
+        }
+        else {
+            node<T>* popped;
+            popped = tail;
+            // TODO check for errors in memory asignment
+            tail = tail->prev();
+            tail->next(NULL);
+            popped->prev(NULL);
+            return popped;
+        }
+    }
+    void pushHead(T data) {
+        insertFront(T data);
+    }
+    node<T>* popHead() {
+        if (isEmpty()) {
+            return NULL;
+        }
+        else {
+            node<T>* popped;
+            popped = head;
+            // TODO check for errors in memory asignment
+            head = head->next();
+            head->prev(NULL);
+            popped->next(NULL);
+            return popped;
         }
     }
 
