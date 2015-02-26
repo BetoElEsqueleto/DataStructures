86ce9688a0fea3efae84949e33b70cec67d0d11d
Alberto Barradas <abcsds@gmail.com>
2015-02-25 23:53:02 -0600

    Added Student Class
    Added student structure class for course test.

--- test/Students/student.cpp
+++ test/Students/student.cpp
@@ -1,5 +1,22 @@
 #include "student.h"
 
-Student::Student()
+Student::Student(int id, string name, int note1, int note2, int grade)
 {
+    self.id = id;
+    self.name = name;
+    self.note1 = note1;
+    self.note2 = note2;
+    self.grade = grade;
 }
+
+int id(){ return id; }
+string name(){ return name; }
+int note1(){ return note1; }
+int note2(){ return note2; }
+int grade(){ return grade; }
+
+void id(int id) {self.id = id;}
+void name(string name) { self.name = name; }
+void note1(int note1) { self.note1 = note1; }
+void note2(int note2) { self.note2 = note2; }
+void grade(int grade) { self.grade = grade; }
--- test/Students/student.h
+++ test/Students/student.h
@@ -1,10 +1,34 @@
 #ifndef STUDENT_H
 #define STUDENT_H
+#include <iostream>
+#include "list.h"
+
+using namespace std;
 
 class Student
 {
+private:
+    int id;
+    string name;
+    int note1;
+    int note2;
+    int grade;
 public:
-    Student();
+    Student(int id, string name, int note1, int note2, int grade);
+    ~Student();
+    
+    int id();
+    string name();
+    int note1();
+    int note2();
+    int grade();
+
+    void id(int);
+    void name(string);
+    void note1(int);
+    void note2(int);
+    void grade(int);
+
 };
 
 #endif // STUDENT_H
